ea89d595497c70cbe303eb9ec55dec77f6a35924
HBK <sammcowen@gmail.com>
2022-01-06 12:04:20 -0800

    created prompts

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Assignment code here
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", ];
+var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upperChar = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", 
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+var lowerChar = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+ "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+var passwordLength = prompt("enter desired password length. Must be between 8 and 128");
 
+function getPasswordFormula()  {
+  var passwordLength =  prompt("Enter desired password character length. Must be between 8 and 128 characters");
+  var upperYes =  window.confirm("Do you want to use uppercase letters?");
+   
+   window.confirm("Do you want to use special characters?");
+    
+     window.confirm("Do you want to use numbers?");
+   window.confirm("Do you want to use lowercase letters?");
+} 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +34,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getPasswordFormula);
