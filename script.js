697fcbe28786d609e32cbe64e59dc789ae0f77a3
HBK <sammcowen@gmail.com>
2022-01-06 17:18:14 -0800

    prompts created. if statements added, to generate random values from arrays

--- script.js
+++ script.js
@@ -9,17 +9,39 @@ var lowerChar = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
-var passwordLength = prompt("enter desired password length. Must be between 8 and 128");
-
+// prompts for password creation
 function getPasswordFormula()  {
   var passwordLength =  prompt("Enter desired password character length. Must be between 8 and 128 characters");
-  var upperYes =  window.confirm("Do you want to use uppercase letters?");
-   
-   window.confirm("Do you want to use special characters?");
-    
-     window.confirm("Do you want to use numbers?");
-   window.confirm("Do you want to use lowercase letters?");
-} 
+  if(passwordLength < 8 || passwordLength > 128) {
+    window.prompt("Please enter a value between 8 and 128!");
+  } 
+  var upperConfirm =  window.confirm("Do you want to include uppercase letters?");  
+  if(upperConfirm) {
+  var randomUpper = upperChar[(Math.random() * upperChar.length) | 0]
+console.log(randomUpper);
+  }  
+  var specialConfirm = window.confirm("Do you want to include special characters?"); 
+  if(specialConfirm) {
+    var randomSpecial = specialChar[(Math.random() * specialChar.length) | 0]
+    console.log(randomSpecial);
+  }
+  var numConfirm = window.confirm("Do you want to include numbers?");
+  if(numConfirm) {
+    var randomNum = numChar[(Math.random() * numChar.length) | 0]
+    console.log(randomNum);
+  }
+  var lowerConfirm = window.confirm("Do you want to include lowercase letters?");
+  if(lowerConfirm) {
+    var randomLower = lowerChar[(Math.random() * lowerChar.length) | 0]
+    console.log(randomLower);
+  }
+   console.log("passwordLength: ", passwordLength);
+   console.log("upperConfirm: ", upperConfirm);
+   console.log("specialConfirm: ", specialConfirm);
+   console.log("numConfirm: ", numConfirm);
+   console.log("lowerConfirm: ", lowerConfirm);
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
